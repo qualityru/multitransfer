85e0ea4d310eb9197302c52f649c4223f36de21a
Quality <qualityru@gmail.com>
2025-12-09 11:23:13 +0300

    fix config

--- config.py
+++ config.py
@@ -8,25 +8,25 @@ ENV_PATH = BASE_DIR / "./.env"
 
 
 class Settings(BaseSettings):
-    DB_HOST: str = Field(..., env="DB_HOST")
-    DB_PORT: int = Field(..., env="DB_PORT")
-    DB_USER: str = Field(..., env="DB_USER")
-    DB_PASS: str = Field(..., env="DB_PASS")
-    DB_NAME: str = Field(..., env="DB_NAME")
+    # DB_HOST: str = Field(..., env="DB_HOST")
+    # DB_PORT: int = Field(..., env="DB_PORT")
+    # DB_USER: str = Field(..., env="DB_USER")
+    # DB_PASS: str = Field(..., env="DB_PASS")
+    # DB_NAME: str = Field(..., env="DB_NAME")
 
-    # REDIS_HOST: str = Field(..., env="REDIS_HOST")
-    # REDIS_PORT: str = Field(..., env="REDIS_PORT")
-    # REDIS_PASS: str = Field(..., env="REDIS_PASS")
+    # # REDIS_HOST: str = Field(..., env="REDIS_HOST")
+    # # REDIS_PORT: str = Field(..., env="REDIS_PORT")
+    # # REDIS_PASS: str = Field(..., env="REDIS_PASS")
 
-    SECRET_KEY: str = Field(..., env="SECRET_KEY")
+    # SECRET_KEY: str = Field(..., env="SECRET_KEY")
 
-    AUTH_TOKEN_EXPIRES: int = Field(..., env="AUTH_TOKEN_EXPIRES")
+    # AUTH_TOKEN_EXPIRES: int = Field(..., env="AUTH_TOKEN_EXPIRES")
 
-    BOT_TOKEN: str = Field(..., env="BOT_TOKEN")
+    # BOT_TOKEN: str = Field(..., env="BOT_TOKEN")
 
-    GUARD_HASH_SECRET: str = Field(..., env="GUARD_HASH_SECRET")
-    SMTP_LOGIN: str = Field(..., env="SMTP_LOGIN")
-    SMTP_PASSWORD: str = Field(..., env="SMTP_PASSWORD")
+    # GUARD_HASH_SECRET: str = Field(..., env="GUARD_HASH_SECRET")
+    # SMTP_LOGIN: str = Field(..., env="SMTP_LOGIN")
+    # SMTP_PASSWORD: str = Field(..., env="SMTP_PASSWORD")
 
     # ADMIN_TOKEN: str = Field(..., env="ADMIN_TOKEN")
 
